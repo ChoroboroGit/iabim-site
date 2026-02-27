66d2732d1b1c8b310d50a288bcdc904460ba3b31
Piotr Choromański <piotrekchoromanski@gmail.com>
2026-02-27 18:51:50 +0100

    Deployed 3264cd59 with MkDocs version: 1.6.1

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,15 +1,27 @@
+// Czekaj na pełne załadowanie strony
+window.addEventListener('load', function() {
+  if (typeof GLightbox !== 'undefined') {
+    GLightbox({
+      selector: '.glightbox',
+      touchNavigation: true,
+      loop: true,
+      closeButton: true,
+      zoomable: true,
+      draggable: true
+    });
+  }
+});
+
+// Dla nawigacji SPA (Material theme)
 document.addEventListener('DOMContentLoaded', function() {
-  const lightbox = GLightbox({
-    selector: '.glightbox',
-    touchNavigation: true,
-    loop: true,
-    closeButton: true,
-    zoomable: true,
-    draggable: true,
-    openEffect: 'fade',
-    closeEffect: 'fade',
-    slideEffect: 'slide',
-    moreLength: 0,
-    preload: true
-  });
+  if (typeof GLightbox !== 'undefined') {
+    GLightbox({
+      selector: '.glightbox',
+      touchNavigation: true,
+      loop: true,
+      closeButton: true,
+      zoomable: true,
+      draggable: true
+    });
+  }
 });
