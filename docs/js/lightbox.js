deded5360a5e412ff1012ec229c8026c55bb5e5c
Piotr Choromański <piotrekchoromanski@gmail.com>
2026-02-27 18:54:51 +0100

    Restore slideEffect

--- docs/js/lightbox.js
+++ docs/js/lightbox.js
@@ -1,27 +1,13 @@
-// Czekaj na pełne załadowanie strony
-window.addEventListener('load', function() {
-  if (typeof GLightbox !== 'undefined') {
-    GLightbox({
-      selector: '.glightbox',
-      touchNavigation: true,
-      loop: true,
-      closeButton: true,
-      zoomable: true,
-      draggable: true
-    });
-  }
-});
-
-// Dla nawigacji SPA (Material theme)
 document.addEventListener('DOMContentLoaded', function() {
-  if (typeof GLightbox !== 'undefined') {
-    GLightbox({
-      selector: '.glightbox',
-      touchNavigation: true,
-      loop: true,
-      closeButton: true,
-      zoomable: true,
-      draggable: true
-    });
-  }
+  const lightbox = GLightbox({
+    selector: '.glightbox',
+    touchNavigation: true,
+    loop: true,
+    closeButton: true,
+    zoomable: true,
+    draggable: true,
+    openEffect: 'fade',
+    closeEffect: 'fade',
+    slideEffect: 'slide'
+  });
 });
