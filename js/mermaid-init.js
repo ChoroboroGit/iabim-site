213c689b0169c0a28c108274afb8a92a478adcc6
Piotr Choromański <piotrekchoromanski@gmail.com>
2026-03-06 15:59:55 +0100

    Deployed 03663501 with MkDocs version: 1.6.1

--- js/mermaid-init.js
+++ js/mermaid-init.js
@@ -14,7 +14,20 @@ function initMermaid() {
         }
     });
 
-    mermaid.initialize({ startOnLoad: false, theme: 'neutral' });
+    mermaid.initialize({
+        startOnLoad: false,
+        theme: 'base',
+        themeVariables: {
+            primaryColor: '#448aff',
+            primaryBorderColor: '#2979ff',
+            primaryTextColor: '#ffffff',
+            lineColor: '#448aff',
+            secondaryColor: '#e3f2fd',
+            tertiaryColor: '#bbdefb',
+            edgeLabelBackground: '#ffffff',
+            nodeTextColor: '#ffffff'
+        }
+    });
     mermaid.init(undefined, '.mermaid');
 }
 
